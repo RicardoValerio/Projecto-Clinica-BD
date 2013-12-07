7e5e54f92da31bb901f3f9bc5dab51a5c5d5aae1
Ricardo Valério <jricvalerio@gmail.com>
2013-12-07 16:00:38 +0000

    update form in 'contactos.php'

---
+++ Project_Clinica/includeFormContacto.php
@@ -0,0 +1,35 @@
+<div class="nine columns">
+
+              <dl class="contained tabs">
+                <dd class="active"><a href="#contactForm">Contacte a Pseudo Clínica</a></dd>
+              </dl>
+        
+                    <ul class="tabs-content contained">
+                      <li id="contactFormTab" class="active">
+
+                          <form action="?#m" method="post">
+                            
+                            <div class="two columns"><label class="inline">Nome:</label></div>
+                            <div class="ten columns "><input type="text" id="Nome_formulario" placeholder="" name="nome"/>
+                            <small id="avisoNome" class="invisivel">Por favor. Introduza um Nome válido...</small>
+                            
+                                        </div>
+                                        <!-- <div class="row collapse"> -->
+                            <div class="two columns">
+                              <label class="inline">Email:</label>
+                            </div>
+                            <div class="ten columns">
+                              <input class="" type="email" id="Email_formulario" placeholder="" name="email"/>
+                              <small id="avisoEmail" class="invisivel">Por favor. Introduza um Email válido...</small>
+                                        </div>
+                                
+                                        <label>A sua Questão:</label>
+                                        <textarea rows="10" maxlength="1000" id="caixaMensagem" name="questao"></textarea>
+                                        
+                                
+                                        <button type="submit" id="botaoFinaliza" class="radius button" disabled>Enviar</button>
+                          </form>
+                      </li>
+                    </ul>
+        
+          </div>
